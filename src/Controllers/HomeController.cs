b0bd922c6298b2f3fb2aeae805ce5752cf8cbe0f
Selmir <hajruli.s@gmail.com>
2017-10-06 19:52:30 +0200

    Moved usings into namespaces

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -1,12 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
 namespace BlhackerNews.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
     public class HomeController : Controller
     {
         public IActionResult Index()
