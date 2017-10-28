939671546c2f4b95da0b5cdf6c19c26f6f3559ed
Roxtar <R0xtar@users.noreply.github.com>
2017-10-28 21:31:14 +0200

    Implemented BenchmarkDotNet to benchmark api requests

---
+++ src/Helpers/Benchmark.cs
@@ -0,0 +1,57 @@
+namespace BlhackerNews.Helpers
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using System.Linq;
+    using BenchmarkDotNet.Attributes;
+    using BlhackerNews.Services;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Jobs;
+
+    [Config(typeof(FastAndDirtyConfig))]
+    public class Benchmark
+    {
+        private NewsService _newsService;
+        public Benchmark()
+        {
+            _newsService = new NewsService();
+        }
+
+        [Benchmark]
+        public void GetLastNews()
+        {
+            var result = _newsService.GetLastNews().Result.ToList();
+        }
+
+        [Benchmark]
+        public void GetLastNews2()
+        {
+            var result = _newsService.GetLastNews2().Result.ToList();
+        }
+
+        [Benchmark]
+        public void GetLastNews3()
+        {
+            var result = _newsService.GetLastNews3().Result.ToList();
+        }
+    }
+
+    public class FastAndDirtyConfig : ManualConfig
+    {
+    public FastAndDirtyConfig()
+    {
+        Add(DefaultConfig.Instance); // *** add default loggers, reporters etc? ***
+
+        Add(Job.Default
+            .WithWarmupCount(3)     // 3 warmup iteration
+            .WithTargetCount(3)     // 3 target iteration
+        );
+    }
+}
+}
